69d085b206cdc5f1ce12d8beaa9f472563b3f2b4
hao <devnull@localhost>
2011-01-23 01:51:38 +0000

    comparison_Yap has diagonistics to check data generation.

--- programs/R/comparison_Yap.R
+++ programs/R/comparison_Yap.R
@@ -1,13 +1,14 @@
 library(qtl)
+library(ggplot2)
 source('logistic.R')
 
 beta.coef <- matrix(c(30, 5.0, 0.5, 28.5, 5.0, 0.5, 27.5, 5., 0.5), byrow=T, nrow=3)
-tt <- 1:10                              # PLACEHOLDER
+tt <- 0.:9
 len.tt <- 10                                 # time series length
 
 structured.cov <- matrix( c(0.72, 0.39, 0.45, 0.48, 0.50, 0.53, 0.60, 0.64, 0.68, 0.68, 0.39, 1.06, 1.61, 1.60, 1.50, 1.48, 1.55, 1.47, 1.35, 1.29, 0.45, 1.61, 3.29, 3.29, 3.17, 3.09, 3.19, 3.04, 2.78, 2.53, 0.48, 1.60, 3.29, 3.98, 4.07, 4.01, 4.17, 4.18, 4.00, 3.69, 0.50, 1.50, 3.17, 4.07, 4.70, 4.68, 4.66, 4.78, 4.70, 4.36, 0.53, 1.48, 3.09, 4.07, 4.68, 5.56, 6.23, 6.87, 7.11, 6.92, 0.60, 1.55, 3.19, 4.17, 4.66, 6.23, 8.59, 10.16, 10.80, 10.70, 0.64, 1.47, 3.04, 4.18, 4.78, 6.87, 10.16, 12.74, 13.80, 13.80, 0.68, 1.35, 2.78, 4.00, 4.70, 7.11, 10.80, 13.80, 15.33, 15.35, 0.68, 1.29, 2.53, 3.69, 4.36, 6.92, 10.70, 13.80, 15.35, 15.77), byrow=T, nrow=10)
 
-## generate data
+## generate both genotypes and phenotypes
 gen.data <- function(sample.size, cov.fcn){
   popu.size <- 10000  # population size
   
@@ -25,14 +26,14 @@ gen.data <- function(sample.size, cov.fcn){
 
   ## simulate phenotypes
   ## 1. get the means
-  mean.vals <- matrix(0., nrow=sample.size, ncol=len.tt)
+  mean.vals <- matrix(0., nrow=3, ncol=len.tt) # only 3 genotypes means only 3 mean curves
   for (i in 1:3){
     mean.vals[i,] <- logisticFun(beta.coef[i,],tt)
   }
   sample.means <- mean.vals[samples$qtlgeno,]
   ## 2. get the noises
   if (cov.fcn == 'autocorr'){           # case (1)
-    ee <- rnormAutocor(sample.size, tt, 0.6, 3.0) # rho = 0.6, sigma^2 = 3.
+    ee <- rnormAutocor(sample.size, tt, 0.6, 3.0) # rho = 0.6, sigma^2 = 3
   }
   else if (cov.fcn == 'equicorr'){      # case (2)
     cov <- matrix(0.5, nrow=len.tt, ncol=len.tt)  # rho = 0.5
@@ -52,5 +53,72 @@ gen.data <- function(sample.size, cov.fcn){
   return(samples)
 }
 
+## plot a set of time series as rows of a matrix.
+## time is the sequence of time points
+## ID is the sample IDs of the time series;
+## color.code is the code that distinguish groups among samples
+plot.ts <- function(ts, time, ID, color.code,...){
+  stopifnot(nrow(ts) == length(ID), length(ID) == length(color.code))
+  stopifnot(ncol(ts) == length(time))
 
+  header <- paste(ID, color.code, sep=' ')
+  tmp <- data.frame(cbind(time,t(ts)), row.names=time)
+  colnames(tmp) <- c('time', header)
+  temp <- melt(tmp, id.vars='time')
+  temp <- cbind(temp, colsplit(temp$variable, ' ', c('ID', 'color.code')))
 
+  return(geom_line(data=temp, aes(time, value,group=ID, color=factor(color.code)),...))
+}
+
+## plot a covariance matrix as a heatmap
+## cov.mat is the covariance matrix to be plotted
+## time is the time points of which we assume the covariance matrix comes from
+## returns a heatmap layer
+plot.hm <- function(cov.mat, time){
+  tmp <- data.frame(cbind(1:10, cov.mat))
+  colnames(tmp) <- c('time', paste(time))
+  tmp <- melt(tmp, id.vars='time')
+  hmp <- geom_tile(data=tmp, mapping=aes(time, variable, fill=value)) 
+  return(hmp)
+}
+## generate a series of diagonistic plots
+diagnostics <- function(){
+  diag.one <- function(cov.fcn){
+    samples <- gen.data(200, cov.fcn)
+    Y <- samples$pheno
+    X <- samples$qtlgeno
+
+    ## compare mean curve against true values
+    lrg <- lm(Y~factor(X)-1)
+    tmp <- fitted(lrg)
+    sam.ave <- rbind(tmp[which(X==1)[1],], tmp[which(X==2)[1],], tmp[which(X==3)[1],])
+    fig.sam <- plot.ts(sam.ave, tt, 1:3, 1:3, linetype=1)
+    mean.vals <- matrix(0., nrow=3, ncol=len.tt) # only 3 genotypes means only 3 mean curves
+    for (i in 1:3){
+      mean.vals[i,] <- logisticFun(beta.coef[i,],tt)
+    }
+    fig.true <- plot.ts(mean.vals, tt, 1:3, 1:3, linetype=2)
+    mean.curves <- ggplot()+fig.sam+fig.true+xlab('time')+ylab('phenotype')+opts(title=paste('mean curves with', cov.fcn))
+    
+    ## look at the heatmap of residual covariance
+    hm <- ggplot() + plot.hm(cov(resid(lrg)), 1:10) + xlab('time') + ylab('time') +opts(title=cov.fcn)
+    
+    return(list(mean=mean.curves, cov=hm))
+  }
+  
+  pdf(file='diagnostics.pdf')
+  autocorr <- diag.one('autocorr')
+  equicorr <- diag.one('equicorr')
+  structured <- diag.one('structured')
+
+  ## plot the figures
+  ## 1. mean curves
+  print(autocorr$mean)
+  print(equicorr$mean)
+  print(structured$mean)
+  ## 2. covariance heatmap
+  print(autocorr$cov)
+  print(equicorr$cov)
+  print(structured$cov)
+  dev.off()
+}
